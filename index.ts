44e1b1008cdbf891468b0263f8ec769a20c19959
RoomofR <alawajiryan@gmail.com>
2025-08-26 12:15:27 +0300

    Added prod/dev port with TLS

--- index.ts
+++ index.ts
@@ -1,16 +1,50 @@
 import { serve } from "bun";
 import { Database } from "bun:sqlite";
+import { resolveTilde } from "bunmagic";
 
-const server = serve({
-	port: 9567,
+const ENV = process.env.BUILD;
+const PORT = ENV === "PROD" ? 443 : Number(process.env.PORT);
+
+const server_options = {
+	port: PORT,
 	fetch(req){
 		return new Response("Bun!");
 	},
-});
+}
+
+//ADD TLS if on production
+if(ENV === "PROD"){
+	server_options.tls = {
+		key: Bun.file(resolveTilde(process.env.TLS_KEY_PATH)),
+    	cert: Bun.file(resolveTilde(process.env.TLS_CERT_PATH)),
+	}
+}
+
+const server = serve(server_options);
 
 console.log(`Listening on localhost:${server.port}`);
 
+//Fetches json from url and parses/returns  ajson object
+async function fetchJSON(url){
+	try{
+		const res = await fetch(url);
+		if(!res.ok){
+			throw new Error(`Request failed: ${res.status}`);
+		}
+		const json = await res.json();
+		return json;
 
+	}catch (err){
+		console.error("Fetch error:", err);
+	}
+}
+
+async function fetchSalesHistory(itemIDs, options){
+
+}
+
+
+//console.log( await fetchJSON(`https://universalis.app/api/v2/history/europe/8?entriesToReturn=99999&minSalePrice=0&maxSalePrice=2147483647`))
 
 
 /*const db = new Database("mydb.sqlite", { create: true });
