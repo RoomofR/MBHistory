357fdbcbb1b35fd2ba13e464063d43e950600aa7
RoomofR <alawajiryan@gmail.com>
2025-08-26 12:30:44 +0300

    tls directory fix

--- index.ts
+++ index.ts
@@ -14,9 +14,11 @@ const server_options = {
 
 //ADD TLS if on production
 if(ENV === "PROD"){
+	console.log(process.env.TLS_KEY_PATH,process.env.TLS_CERT_PATH);
+
 	server_options.tls = {
-		key: Bun.file(resolveTilde(process.env.TLS_KEY_PATH)),
-    	cert: Bun.file(resolveTilde(process.env.TLS_CERT_PATH)),
+		key: Bun.file(process.env.TLS_KEY_PATH),
+    	cert: Bun.file(process.env.TLS_CERT_PATH),
 	}
 }
 
